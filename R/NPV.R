41a745604c89d8b5a7f3262df35524d9b1401f17
amejaes <amejaes@gmail.com>
2020-11-16 11:39:37 -0800

    Updating documentation

--- R/NPV.R
+++ R/NPV.R
@@ -1,3 +1,14 @@
+#' Calculate total NPV
+#'
+#' @param x Numeric vector or list
+#' @param y Numeric vector
+#'
+#' @return
+#' A vector containing the sum of the NPV
+#' @export
+#'
+#' @examples
+#' NPV(1:10, 0.05)
 NPV <- function(x, y) {
   sum(map2_dbl(x, y, ~(.y+1)^.x))
 }
